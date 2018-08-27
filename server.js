0243947657a269d361d2c39b99b8e311dcf8ebec
Dylan Reuter <djreuter45@gmail.com>
2018-08-27 09:12:57 -0600

    added db connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((e) => console.log('Could not connect', e));
 
 app.get('/', (req, res) => {
   res.send('Hello');
