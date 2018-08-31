bebfd0e08cc96282432d65c519a71d5431f575c9
Dylan Reuter <djreuter45@gmail.com>
2018-08-31 15:06:29 -0600

    imported bodyparser

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
