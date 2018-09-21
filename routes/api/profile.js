d60a382f28fc522198b2f20226c01b8bb200cb4c
Dylan Reuter <djreuter45@gmail.com>
2018-09-21 13:55:45 -0600

    added get profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const { Profile } = require('../../models/Profile');
+const { User } = require('../../models/User');
 
 /**
  * @route  GET api/profile/test
@@ -8,6 +13,23 @@ const router = express.Router();
  */
 router.get('/test', (req, res) => {
   return res.json({msg: "profile works"});
+});
+
+/**
+ * @route  GET api/profile
+ * @desc   get current users profile
+ * @access private
+ */
+router.get('/', passport.authenticate('jwt', { session: false }), 
+(req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.user.id }).then((profile) => {
+    if (!profile) {
+      errors.noprofile = 'Profile does not exist'
+      return res.status(404).json(errors);
+    }
+    return res.json(profile);
+  }).catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
