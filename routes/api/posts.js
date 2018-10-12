044ceda8314a1809b90fb66d9f2f96e1d1c05356
Dylan Reuter <djreuter45@gmail.com>
2018-10-12 10:52:03 -0600

    added delete route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+
 const { Post } = require('../../models/Post');
+const { Profile } = require('../../models/Profile');
 const { validatePostInput } = require('../../validation/post');
 
 
@@ -13,13 +15,24 @@ const { validatePostInput } = require('../../validation/post');
  */
 router.get('/', async (req, res) => {
   try {
-    const posts = await Post.find();
-    if (!!posts) {
-      return res.status(404).send('No posts exist')
-    }
+    const posts = await Post.find().sort({date: -1});
     return res.json(posts);
   } catch (err) {
-    return res.status(400).json(err);
+    return res.status(404).json({nopostsfound: 'No posts found'});
+  }
+});
+
+/**
+ * @route  GET api/posts/:id
+ * @desc   get post by id
+ * @access public
+ */
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.json(post);
+  } catch (err) {
+    return res.status(404).json({nopostfound: 'No post found'});
   }
 });
 
@@ -41,6 +54,23 @@ router.post('/', passport.authenticate('jwt', { session: false }),
       user: req.user.id
     });
     newPost.save().then((post) => res.json(post));
+});
+
+/**
+ * @route  POST api/posts/:id
+ * @desc   delete a post
+ * @access private
+ */
+router.delete('/:id', passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    Profile.findOne({user: req.user.id}).then((profile) => {
+      Post.findById(req.params.id).then((post) => {
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({notauthorized: 'User not authorized'});
+        }
+        Post.remove().then(() => res.json({success: true}));
+      });
+    }).catch((err) => res.status(404).json({postnotfound: 'Post not found'}));
 });
 
 module.exports = router;
