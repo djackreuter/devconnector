c5be6091419d304b92175bb66717c57586cda3b6
Dylan Reuter <djreuter45@gmail.com>
2018-10-30 16:19:59 -0500

    added error to state errors object

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -29,7 +29,7 @@ export default class Register extends Component {
     }
     axios.post('/api/users/register', newUser).then((res) => {
       console.log(res);
-    }).catch((err) => console.log(err));
+    }).catch((err) => this.setState({errors: err.response.data}));
   }
 
   render() {
