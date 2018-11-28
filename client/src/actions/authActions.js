bd2b6093ca2e28ef74d781400828493d1572f400
Dylan Reuter <djreuter45@gmail.com>
2018-11-27 20:12:51 -0600

    added registerUser to auth action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,12 @@
-import { TEST_DISPATCH } from './types';
+import axios from 'axios';
+import { GET_ERRORS } from './types';
 
-export const registerUser = (data) => ({
-  type: TEST_DISPATCH,
-  payload: data
-});
+export const registerUser = (userData, history) => dispatch => {
+  axios.post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  );
+};
