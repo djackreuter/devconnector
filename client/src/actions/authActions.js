f832443694f9224da29e22bb68e700efe6af5c22
Dylan Reuter <djreuter45@gmail.com>
2018-11-29 19:03:31 -0600

    started login user action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -10,3 +10,17 @@ export const registerUser = (userData, history) => dispatch => {
     })
   );
 };
+
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      setAuthToken(token);
+    })
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  );
+}
