30b217a5dfb2270f9d936eaa2916e6a6dac57c39
Dylan Reuter <djreuter45@gmail.com>
2018-12-31 15:04:40 -0600

    added create profile and delete profile actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
 
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
@@ -16,9 +16,35 @@ export const getCurrentProfile = () => dispatch => {
   )
 }
 
+export const createProfile = (profileData, history) => dispatch => {
+  axios.post('/api/profile', profileData)
+    .then((res) => history.push('/dashboard'))
+    .catch((err) => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  )
+}
+
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
+  }
+}
+
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This cannot be undone!')) {
+    axios.delete('/api/profile').then((res) => 
+      dispatch({
+      type: SET_CURRENT_USER,
+      payload: {}
+      })
+    ).catch((err) => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
   }
 }
 
