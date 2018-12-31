53842aa1373dcb14d3dd2618879a59b91ed917fc
Dylan Reuter <djreuter45@gmail.com>
2018-12-31 15:05:49 -0600

    started adding items to profile display

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getCurrentProfile } from '../../actions/profileActions';
+import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/spinner';
 import { Link } from 'react-router-dom';
+import ProfileActions from './ProfileActions';
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
   }
+
+  onDeleteClick(e) {
+    e.preventDefault();
+    this.props.deleteAccount();
+  }
+
   render() {
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
@@ -17,7 +24,17 @@ class Dashboard extends Component {
       dashboardContent = <Spinner />
     } else {
       if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>TODO: Display profile</h4>
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">
+              Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+            </p>
+            <ProfileActions />
+            <div style={{ marginBottom: '60px' }}>
+              <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Account</button>
+            </div>
+          </div>
+        )
       } else {
         dashboardContent = (
           <div>
@@ -53,8 +70,9 @@ const mapStateToProps = (state) => ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
