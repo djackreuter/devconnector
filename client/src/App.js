102e74dfa5aed7ba2ffc46935c98436bcf8ee5a1
Dylan Reuter <djreuter45@gmail.com>
2019-01-04 16:47:26 -0600

    added add education component, action, and route

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import CreateProfile from './components/createProfile/CreateProfile';
 import './App.css';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -59,6 +60,10 @@ class App extends Component {
                 <PrivateRoute exact path="/add-experience" component={ 
                   AddExperience
                  } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-education" component={ AddEducation 
+                } />
               </Switch>
             </div>
             <Footer />
