95128f19eb6e6f5511159bfba8f9c72c9e429680
Dylan Reuter <djreuter45@gmail.com>
2019-01-11 16:11:50 -0600

    added get profiles action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER, GET_PROFILES } from './types';
 
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
@@ -14,6 +14,20 @@ export const getCurrentProfile = () => dispatch => {
     payload: {}
     })
   )
+}
+
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios.get('/api/profile/all')
+    .then(res => dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    })
+  )
+  .catch(err => dispatch({
+    type: GET_PROFILES,
+    payload: null
+  }))
 }
 
 export const createProfile = (profileData, history) => dispatch => {
