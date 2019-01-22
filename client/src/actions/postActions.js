23212f0d4b3df658fc8d4c769e735e76c5378d11
Dylan Reuter <djreuter45@gmail.com>
2019-01-22 16:18:09 -0600

    added add like and delete post actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS, POST_LOADING, GET_POSTS } from './types';
+import { ADD_POST, GET_ERRORS, POST_LOADING, GET_POSTS, DELETE_POST } from './types';
 
 export const addPost = postData => dispatch => {
   axios.post('/api/posts', postData)
@@ -31,6 +31,35 @@ export const getPosts = () => dispatch => {
       dispatch({
         type: GET_POSTS,
         payload: null
+      })
+    );
+}
+
+export const deletePost = (id) => dispatch => {
+  axios.delete(`/api/posts/${id}`)
+    .then(res => 
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
+export const addLike = (id) => dispatch => {
+  axios.post(`/api/posts/like/${id}`)
+    .then(res => 
+      dispatch(getPosts())
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
     );
 }
